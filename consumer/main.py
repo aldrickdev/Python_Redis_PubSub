c398b8c9b30b9a10ad73b5d072c41118bf1a517c
Aldrick Castro <aldrickdev@gmail.com>
2022-04-30 14:44:35 +0000

    Added comments

--- consumer/main.py
+++ consumer/main.py
@@ -3,13 +3,31 @@ import redis
 
 
 def getRedisClient():
+    """
+    Function used to get a Redis Client
+    
+    Returns:
+        Redis: The instance of the Redis connection
+    """
+
     return redis.Redis(host="redis_pub_sub", port=6379)
 
 
 def checkForMessage(pubsub :redis.client.PubSub) -> str | None:
+    """This function will use the provided PubSub client to check if a message
+     has been published in the subscribed channel
+
+    Args:
+        pubsub (redis.client.PubSub): The PubSub client
+
+    Returns:
+        str | None: If a message has been published then return it, if not 
+        return None
+    """
     message = pubsub.get_message()
 
     if message:
+        # extracts the data from the message
         data = message.get('data')
 
         # Data was published by the producer, lets return it
@@ -18,6 +36,7 @@ def checkForMessage(pubsub :redis.client.PubSub) -> str | None:
 
         # Successful subscription message
         channel = message.get('channel').decode('utf-8')
+
         print(f"Successfully Subcribed to {channel}")
     
     return None
@@ -26,6 +45,8 @@ def checkForMessage(pubsub :redis.client.PubSub) -> str | None:
 def main():
     client = getRedisClient()
     pubsub = client.pubsub()
+    
+    # Subscribes to the "test" channel
     pubsub.subscribe("test")
 
     while True:
--- producer/main.py
+++ producer/main.py
@@ -1,7 +1,14 @@
 from time import sleep
 import redis
 
-def getRedisClient():
+def getRedisClient() -> Redis:
+    """
+    Function used to get a Redis Client
+    
+    Returns:
+        Redis: The instance of the Redis connection
+    """
+    
     return redis.Redis(host="redis_pub_sub", port=6379)
 
 def main():
@@ -10,6 +17,9 @@ def main():
     while True:
         sleep(2)
         print("Publishing \"Hello world\" to channel \"test\"")
+
+        # Uses the Redis connection to publish the message "Hello World" to the
+        # channel "test"
         client.publish("test", "Hello World")
 
 if __name__ == "__main__":
